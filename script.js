371dffc980567e0fb5ba09945de70fe0ba8b5a45
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-03-04 22:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function isSameType(value1, value2) {
   //your js code here
+	function isSameType(value1, value2) {
+    if (isNaN(value1) && isNaN(value2)) {
+        return true; // Both values are NaN
+    }
+
+    return typeof value1 === typeof value2;
+}
+
+// Test cases
+console.log(isSameType(1, 3));        // true
+console.log(isSameType("hey", "hello"));  // true
+console.log(isSameType(NaN, NaN));    // true
+console.log(isSameType("3", 3));      // false
 }
 
 // do not change the code below.
